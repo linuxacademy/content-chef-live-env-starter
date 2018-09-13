fdafae391de47810f3a99c6681ee2a5174975bfb
Keith Thompson <me@keiththomps.com>
2018-09-13 16:45:14 -0400

    First draft of error handling service

---
+++ error_tracker/error_tracker.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+import json
+import logging
+
+app = Flask(__name__)
+
+@app.route("/report", methods=['POST'])
+def report():
+    if not request.json:
+        abort(400)
+    app.logger.info("error submitted: %s", request.json)
+    return { 'message': 'error received' }
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ error_tracker/wsgi.py
@@ -0,0 +1,4 @@
+from error_tracker import app
+
+if __name__ == '__main__':
+    app.run()
