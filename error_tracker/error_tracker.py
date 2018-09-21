2c9df4e2f50bc2fda9e8bdec0d4993b133b65d68
Keith Thompson <me@keiththomps.com>
2018-09-21 11:00:11 -0400

    Adjust the application to run with gunicorn better

--- error_tracker/error_tracker.py
+++ error_tracker/error_tracker.py
@@ -1,7 +1,4 @@
-from flask import Flask
-
-import json
-import logging
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -10,7 +7,7 @@ def report():
     if not request.json:
         abort(400)
     app.logger.info("error submitted: %s", request.json)
-    return { 'message': 'error received' }
+    return '{"message": "error received"}'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- error_tracker/wsgi.py
+++ error_tracker/wsgi.py
@@ -1,4 +1,10 @@
 from error_tracker import app
 
+import logging
+
+gunicorn_logger = logging.getLogger('gunicorn.error')
+app.logger.handlers = gunicorn_logger.handlers
+app.logger.setLevel(gunicorn_logger.level)
+
 if __name__ == '__main__':
     app.run()
